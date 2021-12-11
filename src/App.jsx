d0ba433b8ea7d6b6745b66811c5a23cfef0f5fe6
unknown <miemail@dominio.com>
2021-12-11 17:28:19 -0500

    v5

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import React, { useState } from 'react'
 import Inicio from './components/Inicio'
 import Otro from './components/Otro'
 
+const esty ={
+    padding: 5
+}
+
 const App = ()=>{
     //estado encargado de nombrar las vistas
     const [pagina, setpagina] = useState('inicio')
 
+    //funcion que verifica el estado
     const estado = ()=>{
         if(pagina === 'inicio'){
            return <Inicio />
@@ -15,9 +20,16 @@ const App = ()=>{
         }
     }
 
+    const topage = (page)=>{
+        //console.log(page);
+        setpagina(page)
+    }
+
     return(
         <div>
-            
+           <a href="#" onClick={()=>{topage('inicio')}} style={esty}>Inicio</a> 
+           <a href="#" onClick={()=>{topage('otro')}} style={esty}>Otro</a> 
+           {estado()}
         </div>
         
     )
